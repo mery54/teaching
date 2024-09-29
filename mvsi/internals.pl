94043da6986c9e90bbfa8827d83f5f4b6fcd4b06
Dominique Méry <mery@macdom.local>
2024-09-29 22:16:03 +0200

    I commit

---
+++ mvsi/internals.pl
@@ -0,0 +1,54 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_abrial2010/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-b/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-eatcs/;
+$ref_files{$key} = "$dir".q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$ref_files{$key} = "$dir".q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$ref_files{$key} = "$dir".q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$ref_files{$key} = "$dir".q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ mvsi/labels.pl
@@ -0,0 +1,97 @@
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_abrial2010/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-b/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-eatcs/;
+$external_labels{$key} = "$URL/" . q|node25.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_labels{$key} = "$URL/" . q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_labels{$key} = "$URL/" . q|node16.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2024 (Released January 1, 2024)
+# labels from external_latex_labels array.
+
+
+$key = q/sec:course-mcfsi-at/;
+$external_latex_labels{$key} = q|2 Course MCFSI at Telecom Nancy|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_latex_labels{$key} = q|3 Course Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_latex_labels{$key} = q|4.3 Event-B Archives for the tutorial 2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_latex_labels{$key} = q|4 Event-B Models|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_latex_labels{$key} = q|2.1.3 Lecture 3 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$external_latex_labels{$key} = q|2.1.4 Lecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect3.pdf">Access Control </a>|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_latex_labels{$key} = q|2.3 Project|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_latex_labels{$key} = q|2.1 Slides for the course MCFSI|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_latex_labels{$key} = q|3.1 Slides of the course|; 
+$noresave{$key} = "$nosave";
+
+1;
+
