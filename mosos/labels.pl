d4766a59890c71c8603dbb002358a6c6fa9c03a7
Dominique MERY <dominique.mery@free.fr>
2024-11-08 13:54:36 +0100

    I commit

--- mosos/labels.pl
+++ mosos/labels.pl
@@ -65,44 +65,48 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
+$key = q/cha:un-chap/;
+$external_latex_labels{$key} = q|x1-390002 x1-390002:autorefchapterdeux chap|; 
+$noresave{$key} = "$nosave";
+
 $key = q/sec:course-discont/;
-$external_latex_labels{$key} = q|x1-380004 x1-380004:autorefsectionCourse DISCONT: Modelling hybrid systems|; 
+$external_latex_labels{$key} = q|x1-400002.1 x1-400002.1:autorefsectionCourse DISCONT: Modelling hybrid systems|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-mcfsi-at/;
-$external_latex_labels{$key} = q|x1-30002 x1-30002:autorefsectionCourse MCFSI at Telecom Nancy|; 
+$external_latex_labels{$key} = q|x1-40001.2 x1-40001.2:autorefsectionCourse MCFSI at Telecom Nancy|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-modell-verify/;
-$external_latex_labels{$key} = q|x1-190003 x1-190003:autorefsectionCourse MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
+$external_latex_labels{$key} = q|x1-200001.3 x1-200001.3:autorefsectionCourse MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:documentation/;
-$external_latex_labels{$key} = q|x1-390004.1 x1-390004.1:autorefsubsectionDocumentation|; 
+$external_latex_labels{$key} = q|x1-410002.1.1 x1-410002.1.1:autorefsubsectionDocumentation|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:event-b-archives/;
-$external_latex_labels{$key} = q|x1-300003.3.3 x1-300003.3.3:autorefsubsubsectionEvent-B Archives for the tutorial 2|; 
+$external_latex_labels{$key} = q|x1-31000doc x1-31000doc:autorefsubsubsectionEvent-B Archives for the tutorial 2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:event-b-models/;
-$external_latex_labels{$key} = q|x1-270003.3 x1-270003.3:autorefsubsectionEvent-B Models|; 
+$external_latex_labels{$key} = q|x1-280001.3.3 x1-280001.3.3:autorefsubsectionEvent-B Models|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:lect-3-hrefhttp:m/;
-$external_latex_labels{$key} = q|x1-80002.1.4 x1-80002.1.4:autorefsubsubsectionLecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>.|; 
+$external_latex_labels{$key} = q|x1-9000doc x1-9000doc:autorefsubsubsectionLecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>.|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:project/;
-$external_latex_labels{$key} = q|x1-370003.4 x1-370003.4:autorefsubsectionProject|; 
+$external_latex_labels{$key} = q|x1-380001.3.4 x1-380001.3.4:autorefsubsectionProject|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides/;
-$external_latex_labels{$key} = q|x1-40002.1 x1-40002.1:autorefsubsectionSlides for the course MCFSI|; 
+$external_latex_labels{$key} = q|x1-50001.2.1 x1-50001.2.1:autorefsubsectionSlides for the course MCFSI|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides-course/;
-$external_latex_labels{$key} = q|x1-200003.1 x1-200003.1:autorefsubsectionSlides of the course|; 
+$external_latex_labels{$key} = q|x1-210001.3.1 x1-210001.3.1:autorefsubsectionSlides of the course|; 
 $noresave{$key} = "$nosave";
 
 1;
