e6118b763ce55723baca76fc12bdf39a4be0fbf0
Dominique MERY <dominique.mery@free.fr>
2024-11-20 09:07:28 +0100

    I commit

--- mosos/labels.pl
+++ mosos/labels.pl
@@ -69,5 +69,49 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
+$key = q/sec:course-discont/;
+$external_latex_labels{$key} = q|x15-390004 x15-390004:autorefsectionCourse DISCONT: Modelling hybrid systems|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$external_latex_labels{$key} = q|x4-30002 x4-30002:autorefsectionCourse MCFSI at Telecom Nancy|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_latex_labels{$key} = q|x10-200003 x10-200003:autorefsectionCourse MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:documentation/;
+$external_latex_labels{$key} = q|x16-400004.1 x16-400004.1:autorefsubsectionDocumentation|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_latex_labels{$key} = q|x13-310003.3.3 x13-310003.3.3:autorefsubsubsectionEvent-B Archives for the tutorial 2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_latex_labels{$key} = q|x13-280003.3 x13-280003.3:autorefsubsectionEvent-B Models|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_latex_labels{$key} = q|x5-80002.1.4 x5-80002.1.4:autorefsubsubsectionLecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>.|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:past-exams-course/;
+$external_latex_labels{$key} = q|x9-190002.5 x9-190002.5:autorefsubsectionPast exams of the course MCFSI|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_latex_labels{$key} = q|x14-380003.4 x14-380003.4:autorefsubsectionProject|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_latex_labels{$key} = q|x5-40002.1 x5-40002.1:autorefsubsectionSlides for the course MCFSI|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_latex_labels{$key} = q|x11-210003.1 x11-210003.1:autorefsubsectionSlides of the course|; 
+$noresave{$key} = "$nosave";
+
 1;
 
