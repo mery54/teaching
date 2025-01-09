ca33f37f9c5602d745c8f050e71e3ef6260d1f8e
Dominique MERY <dominique.mery@free.fr>
2025-01-09 23:17:05 +0100

    I commit

---
+++ movex/internals.pl
@@ -0,0 +1,66 @@
+# LaTeX2HTML 2024.2 (Released October 21, 2024)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_abrial2010/;
+$ref_files{$key} = "$dir".q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-b/;
+$ref_files{$key} = "$dir".q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-eatcs/;
+$ref_files{$key} = "$dir".q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-discont/;
+$ref_files{$key} = "$dir".q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$ref_files{$key} = "$dir".q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:documentation/;
+$ref_files{$key} = "$dir".q|node44.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$ref_files{$key} = "$dir".q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$ref_files{$key} = "$dir".q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:past-exams-course/;
+$ref_files{$key} = "$dir".q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$ref_files{$key} = "$dir".q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$ref_files{$key} = "$dir".q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ movex/labels.pl
@@ -0,0 +1,85 @@
+# LaTeX2HTML 2024.2 (Released October 21, 2024)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_abrial2010/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-b/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_losl-eatcs/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-discont/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-mcfsi-at/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:course-modell-verify/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:documentation/;
+$external_labels{$key} = "$URL/" . q|node44.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-archives/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_labels{$key} = "$URL/" . q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-3-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:lect-4-hrefhttp:m/;
+$external_labels{$key} = "$URL/" . q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:past-exams-course/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_labels{$key} = "$URL/" . q|node4.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides-course/;
+$external_labels{$key} = "$URL/" . q|node21.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2024.2 (Released October 21, 2024)
+# labels from external_latex_labels array.
+
+
+$key = q/sec:course-mcfsi-at/;
+$external_latex_labels{$key} = q|x4-30002 x4-30002:autorefsectionCourse MALG1/MOVEX1 at Telecom Nancy|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:project/;
+$external_latex_labels{$key} = q|x7-70002.3 x7-70002.3:autorefsubsectionAssessment|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:slides/;
+$external_latex_labels{$key} = q|x5-40002.1 x5-40002.1:autorefsubsectionSlides for the course MALG1/MOVEX1|; 
+$noresave{$key} = "$nosave";
+
+1;
+
