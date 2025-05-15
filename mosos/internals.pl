199bdd5f53bec1d1f3bf5e84de84a200df329e0f
Dominique MERY <dominique.mery@free.fr>
2025-05-15 16:09:50 +0200

    I commit

--- mosos/internals.pl
+++ mosos/internals.pl
@@ -3,43 +3,43 @@
 
 
 $key = q/cite_abrial2010/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_losl-b/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_losl-eatcs/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-discont/;
-$ref_files{$key} = "$dir".q|node43.html|; 
+$ref_files{$key} = "$dir".q|node55.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-mcfsi-at/;
-$ref_files{$key} = "$dir".q|node47.html|; 
+$ref_files{$key} = "$dir".q|node15.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-modell-verify/;
-$ref_files{$key} = "$dir".q|node20.html|; 
+$ref_files{$key} = "$dir".q|node32.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:documentation/;
-$ref_files{$key} = "$dir".q|node44.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:event-b-archives/;
-$ref_files{$key} = "$dir".q|node58.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:event-b-models/;
 $ref_files{$key} = "$dir".q|node56.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/sec:event-b-archives/;
+$ref_files{$key} = "$dir".q|node50.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$ref_files{$key} = "$dir".q|node40.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/sec:lect-3-hrefhttp:m/;
-$ref_files{$key} = "$dir".q|node52.html|; 
+$ref_files{$key} = "$dir".q|node20.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:lect-4-hrefhttp:m/;
@@ -47,23 +47,23 @@ $ref_files{$key} = "$dir".q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/sec:past-exams-course/;
-$ref_files{$key} = "$dir".q|node42.html|; 
+$ref_files{$key} = "$dir".q|node54.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:project/;
-$ref_files{$key} = "$dir".q|node41.html|; 
+$ref_files{$key} = "$dir".q|node53.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides/;
-$ref_files{$key} = "$dir".q|node48.html|; 
+$ref_files{$key} = "$dir".q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides-course/;
-$ref_files{$key} = "$dir".q|node21.html|; 
+$ref_files{$key} = "$dir".q|node33.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:tuthrefhttp:m/;
-$ref_files{$key} = "$dir".q|node53.html|; 
+$ref_files{$key} = "$dir".q|node9.html|; 
 $noresave{$key} = "$nosave";
 
 1;
--- mosos/labels.pl
+++ mosos/labels.pl
@@ -3,43 +3,43 @@
 
 
 $key = q/cite_abrial2010/;
-$external_labels{$key} = "$URL/" . q|node42.html|; 
+$external_labels{$key} = "$URL/" . q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_losl-b/;
-$external_labels{$key} = "$URL/" . q|node42.html|; 
+$external_labels{$key} = "$URL/" . q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/cite_losl-eatcs/;
-$external_labels{$key} = "$URL/" . q|node42.html|; 
+$external_labels{$key} = "$URL/" . q|node59.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-discont/;
-$external_labels{$key} = "$URL/" . q|node43.html|; 
+$external_labels{$key} = "$URL/" . q|node55.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-mcfsi-at/;
-$external_labels{$key} = "$URL/" . q|node47.html|; 
+$external_labels{$key} = "$URL/" . q|node15.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-modell-verify/;
-$external_labels{$key} = "$URL/" . q|node20.html|; 
+$external_labels{$key} = "$URL/" . q|node32.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:documentation/;
-$external_labels{$key} = "$URL/" . q|node44.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:event-b-archives/;
-$external_labels{$key} = "$URL/" . q|node58.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:event-b-models/;
 $external_labels{$key} = "$URL/" . q|node56.html|; 
 $noresave{$key} = "$nosave";
 
+$key = q/sec:event-b-archives/;
+$external_labels{$key} = "$URL/" . q|node50.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:event-b-models/;
+$external_labels{$key} = "$URL/" . q|node40.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/sec:lect-3-hrefhttp:m/;
-$external_labels{$key} = "$URL/" . q|node52.html|; 
+$external_labels{$key} = "$URL/" . q|node20.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:lect-4-hrefhttp:m/;
@@ -47,23 +47,23 @@ $external_labels{$key} = "$URL/" . q|node9.html|;
 $noresave{$key} = "$nosave";
 
 $key = q/sec:past-exams-course/;
-$external_labels{$key} = "$URL/" . q|node42.html|; 
+$external_labels{$key} = "$URL/" . q|node54.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:project/;
-$external_labels{$key} = "$URL/" . q|node41.html|; 
+$external_labels{$key} = "$URL/" . q|node53.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides/;
-$external_labels{$key} = "$URL/" . q|node48.html|; 
+$external_labels{$key} = "$URL/" . q|node16.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides-course/;
-$external_labels{$key} = "$URL/" . q|node21.html|; 
+$external_labels{$key} = "$URL/" . q|node33.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:tuthrefhttp:m/;
-$external_labels{$key} = "$URL/" . q|node53.html|; 
+$external_labels{$key} = "$URL/" . q|node9.html|; 
 $noresave{$key} = "$nosave";
 
 1;
@@ -74,51 +74,51 @@ $noresave{$key} = "$nosave";
 
 
 $key = q/sec:course-discont/;
-$external_latex_labels{$key} = q|4 Course DISCONT: Modelling hybrid systems|; 
+$external_latex_labels{$key} = q|x20-550005 x20-550005:autorefsectionCourse DISCONT: Modelling hybrid systems|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-mcfsi-at/;
-$external_latex_labels{$key} = q|5 Course EMU Event-B at Maynooth University|; 
+$external_latex_labels{$key} = q|x8-150003 x8-150003:autorefsectionCourse MCFSI at Telecom Nancy|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:course-modell-verify/;
-$external_latex_labels{$key} = q|3 Course MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
+$external_latex_labels{$key} = q|x14-320004 x14-320004:autorefsectionCourse MsC in Computer Science: Modelling and verifying software-based systems for Master in Compurer Science of the University of Lorraine|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:documentation/;
-$external_latex_labels{$key} = q|4.1 Documentation|; 
+$external_latex_labels{$key} = q|x21-560005.1 x21-560005.1:autorefsubsectionDocumentation|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:event-b-archives/;
-$external_latex_labels{$key} = q|5.3.2 Event-B Archives for the tutorials|; 
+$external_latex_labels{$key} = q|x17-500004.3.4 x17-500004.3.4:autorefsubsubsectionEvent-B Archives for the tutorial 3|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:event-b-models/;
-$external_latex_labels{$key} = q|5.3 Event-B Models|; 
+$external_latex_labels{$key} = q|x17-400004.3 x17-400004.3:autorefsubsectionEvent-B Models|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:lect-3-hrefhttp:m/;
-$external_latex_labels{$key} = q|5.1.4 Lecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-maynooth-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>|; 
+$external_latex_labels{$key} = q|x9-200003.1.4 x9-200003.1.4:autorefsubsubsectionLecture 4 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-lect2.pdf">Correctness by Construction with the Modelling Language Event-B using the Refinement</a>.|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:past-exams-course/;
-$external_latex_labels{$key} = q|3.5 Past exams of the Course MsC in Computer Science: Modelling and verifying software-based systems for Master in Comp|; 
+$external_latex_labels{$key} = q|x19-540004.5 x19-540004.5:autorefsubsectionPast exams of the Course MsC in Computer Science: Modelling and verifying software-based systems for Master in Comp|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:project/;
-$external_latex_labels{$key} = q|3.4 Project|; 
+$external_latex_labels{$key} = q|x18-530004.4 x18-530004.4:autorefsubsectionProject|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides/;
-$external_latex_labels{$key} = q|5.1 Slides for the course EMU|; 
+$external_latex_labels{$key} = q|x9-160003.1 x9-160003.1:autorefsubsectionSlides for the course MCFSI|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:slides-course/;
-$external_latex_labels{$key} = q|3.1 Slides of the course|; 
+$external_latex_labels{$key} = q|x15-330004.1 x15-330004.1:autorefsubsectionSlides of the course|; 
 $noresave{$key} = "$nosave";
 
 $key = q/sec:tuthrefhttp:m/;
-$external_latex_labels{$key} = q|5.1.5 Tutorial 1 and 2 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-maynooth-tut.pdf">Tutorial Notes</a>|; 
+$external_latex_labels{$key} = q|x5-90002.1.5 x5-90002.1.5:autorefsubsubsectionTutorial 1 and 2 <a href="http://mery54.github.io/teaching/mosos/lecturesnotes/mcfsi-maynooth-tut.pdf">Tutorial Notes</a>.|; 
 $noresave{$key} = "$nosave";
 
 1;
